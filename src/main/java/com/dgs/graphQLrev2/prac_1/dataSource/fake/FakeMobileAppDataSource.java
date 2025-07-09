5576c1773c7fa16c877ebd8c1d131f592ceedfab
Pranay1012904 <pranay.dmnsns@gmail.com>
2025-07-09 13:50:39 +0530

    Added ID and Extended Scalars

--- src/main/java/com/dgs/graphQLrev2/prac_1/dataSource/fake/FakeMobileAppDataSource.java
+++ src/main/java/com/dgs/graphQLrev2/prac_1/dataSource/fake/FakeMobileAppDataSource.java
@@ -3,13 +3,18 @@ package com.dgs.graphQLrev2.prac_1.dataSource.fake;
 import com.microservices.api_gateway.codegen.types.Address;
 import com.microservices.api_gateway.codegen.types.Author;
 import com.microservices.api_gateway.codegen.types.MobileApp;
+import com.microservices.api_gateway.codegen.types.MobileAppCategory;
 import jakarta.annotation.PostConstruct;
 import net.datafaker.Faker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Configuration
@@ -21,8 +26,8 @@ public class FakeMobileAppDataSource {
     public static final List<MobileApp> MobileAppList = new ArrayList<>();
 
     @PostConstruct
-    public void getMobileApp() {
-        var addresses= new ArrayList<Address>();
+    public void getMobileApp() throws MalformedURLException {
+        var addresses = new ArrayList<Address>();
         for (int i = 0; i < 10; i++) {
 
             var author = Author.newBuilder()
@@ -39,17 +44,22 @@ public class FakeMobileAppDataSource {
             addresses.add(address);
 
             var mobileApp = MobileApp.newBuilder()
+                    .appID(UUID.randomUUID().toString())
                     .name(faker.app().name())
                     .version(faker.app().version())
                     .author(author)
                     .platform(getMobileAppPlatform())
+                    .releaseDate(LocalDate.now().minusDays(5))
+                    .downloaded(faker.random().nextInt(1, 10000))
+                    .homepage(new URL("https://" + faker.internet().url()))
+                    .category(MobileAppCategory.values()[faker.random().nextInt(MobileAppCategory.values().length)])
                     .build();
             MobileAppList.add(mobileApp);
         }
     }
 
     private List<String> getMobileAppPlatform() {
-        switch (ThreadLocalRandom.current().nextInt(1, 12)%3) {
+        switch (ThreadLocalRandom.current().nextInt(1, 12) % 3) {
             case 0:
                 return List.of("Android");
             case 1:
